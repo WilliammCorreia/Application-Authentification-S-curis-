8090e18d97aba8831879958c4578dc77eeea070b
William Correia <william.correia@edu.ece.fr>
2025-05-09 15:27:30 +0200

    ajout image frontend

--- back-end/server.js
+++ back-end/server.js
@@ -11,7 +11,7 @@ const prisma = new PrismaClient();
 const app = express();
 
 app.use(cors({
-    origin: "http://127.0.0.1:5500",
+    origin: ["http://frontend:8080", "http://127.0.0.1:5500"],
     methods: ["GET", "POST"],
     allowedHeaders: ["Content-Type"],
     credentials: true
